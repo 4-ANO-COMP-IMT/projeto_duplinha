e4b05088f24feba21df5b3e42b9f2be19e971cc0
Júlia Galhardi <99993366+JuliaGalhardi@users.noreply.github.com>
2024-06-02 14:49:07 -0300

    refactor(index): update button and horse icon

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,11 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import Cavalo from "./Cavalo"
 import Cartao from "./Cartao"
-import VerCavalo from "/.VerCavalo"
+import VerCavalo from "./VerCavalo"
 
 const App = () => {
+  const funcaoVerCavalo = () => alert('[Popup com informações completas do cavalo]')
   const textoBotao = "Ver Cavalo"
-  const componenteVerCavalo = <VerCavalo textoBotao={textoBotao} />
+  const componenteVerCavalo = <VerCavalo textoBotao={textoBotao} funcaoVerCavalo={funcaoVerCavalo} />
 
   const nome1 = "Pingo"
   const baia1 = "7A"
@@ -18,47 +19,47 @@ const App = () => {
   const pelagem2 = "Alazã Amarilho"
 
   return (
-    //container principal
-    <div className="container border rounded mt-2">
-      {/* linha para o título */}
-      <div className="row border-bottom m-2">
-        <h1 className="display-5 text-center">Cavalos</h1>
-      </div>
+     //container principal
+     <div className="container border rounded mt-2">
+     {/* linha para o título */}
+     <div className="row border-bottom m-2">
+       <h1 className="display-5 text-center">Cavalos</h1>
+     </div>
 
-      {/* linha para o primeiro cavalo */}
-      <div className="row">
-        {/* controle de colunas para responsividade */}
-        <div className="col-sm-8 col-md-6 m2">
-          <Cartao>
-            {/*Cavalo é o props.children do Cartao */}
-            <Cavalo 
-            icone="fa-solid fa-horse-head"
-            nome={nome1}
-            baia={baia1}
-            pelagem={pelagem1}
-            />
-            {/*Mais um que será passado via children é o componenteVerCavalo */}
-            {componenteVerCavalo}
-          </Cartao>
-        </div>
-      </div>
-      {/* linha para o segundo cavalo */}
-      <div className="row">
-        {/* controle de colunas para responsividade */}
-        <div className="col-sm-8 col-md-6 m2">
-          <Cartao>
-            <Cavalo 
-            icone="fa-solid fa-horse-head"
-            nome={nome2}
-            baia={baia2}
-            pelagem={pelagem2}
-            />
-            {/*Mais um que será passado via children é o componenteVerCavalo */}
-            {componenteVerCavalo}
-          </Cartao>
-        </div>
-      </div>
-    </div>
+     {/* linha para o primeiro cavalo */}
+     <div className="row">
+       {/* controle de colunas para responsividade */}
+       <div className="col-sm-8 col-md-6 m2">
+         <Cartao>
+           {/*Cavalo é o props.children do Cartao */}
+           <Cavalo 
+           icone="fa-solid fa-horse-head fa-2x"
+           nome={nome1}
+           baia={baia1}
+           pelagem={pelagem1}
+           />
+           {/*Mais um que será passado via children é o componenteVerCavalo */}
+           {componenteVerCavalo}
+         </Cartao>
+       </div>
+     </div>
+     {/* linha para o segundo cavalo */}
+     <div className="row">
+       {/* controle de colunas para responsividade */}
+       <div className="col-sm-8 col-md-6 m2">
+         <Cartao>
+           <Cavalo 
+           icone="fa-solid fa-horse-head fa-2x"
+           nome={nome2}
+           baia={baia2}
+           pelagem={pelagem2}
+           />
+           {/*Mais um que será passado via children é o componenteVerCavalo */}
+           {componenteVerCavalo}
+         </Cartao>
+       </div>
+     </div>
+   </div>
   );
 };
 

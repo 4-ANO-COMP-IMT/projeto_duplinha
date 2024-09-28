220d54a3041fc849466156498d9fca1df1035ca0
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2024-09-28 13:35:34 -0300

    refactor(barramento-de-eventos): alter cavalos and proprietarios url to their clusterip-service

--- Back-End/barramento-de-eventos/index.js
+++ Back-End/barramento-de-eventos/index.js
@@ -7,9 +7,9 @@ app.use(express.json())
 app.post('/eventos', (req, res) => {
     const evento = req.body
     //envia o evento para o microsserviço de cavalos
-    axios.post('http://192.168.56.1:4000/eventos', evento)
+    axios.post('http://cavalos-clusterip-service:4000/eventos', evento)
     //envia o evento para o microsserviço de proprietários
-    axios.post('http://192.168.56.1:5000/eventos', evento)
+    axios.post('http://proprietarios.clusterip-service:5000/eventos', evento)
     res.status(200).send({msg: 'ok'})
 })
 

5f6e825cccdf82c879e50120c8ec5188bae513f4
Júlia Galhardi <juliagalhardi.c@gmail.com>
2024-10-20 16:10:28 -0300

    refactor(main): remove home page from main and adds router

--- front_flutter/lib/main.dart
+++ front_flutter/lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'src/pages/exibe_cavalos.dart';
+import 'src/pages/exibe_proprietarios.dart';
+import 'src/pages/cadastra_cavalo.dart'; 
+import 'src/pages/cadastra_proprietario.dart'; 
+import 'src/components/cartao.dart';
+import 'routes.dart';
 
 void main() {
-  var app = App();
-  runApp(app);
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cavalos',
+      home: ExibeCavalos(),
+      routes: AppRoutes.getRoutes(),
+    );
+  }
 }

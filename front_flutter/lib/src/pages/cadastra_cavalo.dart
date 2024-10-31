3f7f929700718060fb4d114ba027bb8847813bcc
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2024-10-31 17:07:28 -0300

    feat(cadastra_cavalo): add screen to register horses with reference code

---
+++ front_flutter/lib/src/pages/cadastra_cavalo.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
+
+class CadastraCavalo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+    return Container(
+    //20 pixels de margem esquerda, direita, em cima e embaixo
+    margin: EdgeInsets.all(20.0),
+    child: Column (
+      children: [
+        emailField(bloc),
+        passwordField(bloc),
+        Container(
+          margin: EdgeInsets.only(top: 12.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: submitButton(bloc)
+                ),
+              ],
+            ),
+        ),
+      ],
+    ),
+    );
+  }
+
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      //Stream que, quando atualizado, produz um snapshot
+      //Observe como usamos o Stream definido no bloco
+      stream: bloc.email,
+      //Função que, quando chamada, causa a atualização do Widget
+      //Empacotado pelo Stream Builder
+      builder: ((context, AsyncSnapshot<String> snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            //Dica que aparece quando o usuário clica
+            hintText: "seu@email.com",
+            //Rótulo flutuante
+            labelText: "Endereço de e-mail",
+            //O erro não necessariamente é String, por isso o seu tipo é Object? e usamos o toString
+            errorText: snapshot.hasError ? snapshot.error.toString() : null,
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, AsyncSnapshot<String> snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            hintText: "Senha",
+            labelText: "Senha",
+            errorText: snapshot.hasError? snapshot.error.toString() : null,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.emailPasswordAreOkay,
+      builder: (context, AsyncSnapshot<bool> snapshot){
+         return ElevatedButton(
+          onPressed: snapshot.hasError ? null : (){}, //ainda não temos o que fazer, função vazia
+          child: Text('Login')
+        );
+      },
+    );
+  }
+}
+

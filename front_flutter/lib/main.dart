de0f551df97aa406c8ed70329df0239c8bd11d31
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2024-11-02 10:31:17 -0300

    cleanup(main): remove unnecessary imports

--- front_flutter/lib/main.dart
+++ front_flutter/lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:front_flutter/src/blocs/provider.dart';
-import 'src/pages/exibe_cavalos.dart';
-import 'src/pages/exibe_proprietarios.dart';
-import 'src/pages/cadastra_cavalo.dart'; 
-import 'src/pages/cadastra_proprietario.dart'; 
-import 'src/components/cartao.dart';
 import 'routes.dart';
 import 'src/themes/main_theme.dart';
-import 'src/blocs/bloc.dart';
 
 void main() {
   runApp(MyApp());

c7b5ecd18937f949df39fbdfd6c4925648a9c124
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2024-11-02 17:13:02 -0300

    refactor(validators): add number validations for chip and registry number

--- front_flutter/lib/src/blocs/validators.dart
+++ front_flutter/lib/src/blocs/validators.dart
@@ -29,4 +29,24 @@ mixin Validators {
         sink.add(texto);
     }
   );
+
+  final validateNumeroRegistro = StreamTransformer<String, String>.fromHandlers(
+    handleData: (num_reg, sink) {
+      if (num_reg.length == 6 && RegExp(r'^[0-9]+$').hasMatch(num_reg)) {
+        sink.add(num_reg);
+      } else {
+        sink.addError("O número de registro possui 6 dígitos");
+      }
+    }
+  );
+
+  final validateChip = StreamTransformer<String, String>.fromHandlers(
+    handleData: (num_chip, sink) {
+      if (num_chip.length == 15 && RegExp(r'^[0-9]+$').hasMatch(num_chip)) {
+        sink.add(num_chip);
+      } else {
+        sink.addError("O número do chip possui 15 dígitos");
+      }
+    }
+  );
 }

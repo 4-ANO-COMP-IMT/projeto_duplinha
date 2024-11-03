cf89c09873f0ff4b5cc200c736217f9908776ecc
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2024-11-03 17:30:05 -0300

    style(main_theme): alter appBar and icons

--- front_flutter/lib/src/themes/main_theme.dart
+++ front_flutter/lib/src/themes/main_theme.dart
@@ -22,7 +22,7 @@ class MainTheme {
         ),
         shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
           RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8)), // Bordas arredondadas
+            borderRadius: BorderRadius.all(Radius.circular(12)), // Bordas arredondadas
           ),
         ),
       ),
@@ -31,7 +31,14 @@ class MainTheme {
 
     ),
     appBarTheme: const AppBarTheme(
-
+      titleTextStyle: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
     ),
+    iconTheme: IconThemeData(
+      color: Color(0xFF1ABC9C),
+    ),
+    
   );
 }

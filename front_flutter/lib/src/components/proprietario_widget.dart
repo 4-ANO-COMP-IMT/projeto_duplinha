23a34fc9c9f14f95c37fbcb6de1f0a358f87818f
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2024-11-03 15:09:06 -0300

    refactor(proprietario_widget): add widget to show owner's info on card

---
+++ front_flutter/lib/src/components/proprietario_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ProprietarioWidget extends StatelessWidget {
+  final String nome;
+  final String telefone;
+  final String email;
+
+  const ProprietarioWidget({
+    required this.nome,
+    required this.telefone,
+    required this.email,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(nome, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        SizedBox(height: 8),
+        Text("Telefone: $telefone", style: TextStyle(fontSize: 16)),
+        Text("E-mail: $email", style: TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+}

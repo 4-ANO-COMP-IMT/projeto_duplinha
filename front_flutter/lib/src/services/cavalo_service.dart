0736f4050fab50c05464826d9669f196355cfc67
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2024-11-03 18:25:10 -0300

    feat(cavalo_service): add fetchCavalo method

--- front_flutter/lib/src/services/cavalo_service.dart
+++ front_flutter/lib/src/services/cavalo_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/cavalo_raw.dart';
+import '../models/cavalo.dart';
 
 class CavaloService {
   final String apiUrl = 'http://localhost:31661/cavalos';
@@ -24,6 +25,36 @@ class CavaloService {
         castrado: item['infos']['castrado'],
         proprietarios: List<String>.from(item['infos']['id_proprietarios'] ?? []),
       )).toList();
+    } else {
+      throw Exception('Falha ao carregar dados dos cavalos');
+    }
+  }
+
+  Future<Cavalo?> fetchCavalo(idCavalo) async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      List<CavaloRaw>listaCavalos = data.map((item) => CavaloRaw(
+        id: item['idCavalo'],
+        nome: item['infos']['nome'],
+        baia: item['infos']['baia'],
+        pelagem: item['infos']['pelagem'],
+        dataNascimento: item['infos']['dt_nasc'],
+        sexo: item['infos']['sexo'],
+        numReg: item['infos']['num_reg'],
+        tipoReg: item['infos']['tipo_reg'],
+        chip: item['infos']['num_chip'],
+        raca: item['infos']['raca'],
+        castrado: item['infos']['castrado'],
+        proprietarios: List<String>.from(item['infos']['id_proprietarios'] ?? []),
+      )).toList();
+
+      for(final cavalo in listaCavalos){
+        if(cavalo.id == idCavalo){
+          return Cavalo(cavalo);
+        }
+      }
     } else {
       throw Exception('Falha ao carregar dados dos cavalos');
     }
